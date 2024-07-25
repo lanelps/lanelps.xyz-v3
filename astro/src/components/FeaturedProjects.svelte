6171ef26c81a67d60175f067d762f5e1a29c6852
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-07-26 00:11:33 +1000

    refactor: Update FeaturedProjects component to use mouse coordinates for animation

--- astro/src/components/FeaturedProjects.svelte
+++ astro/src/components/FeaturedProjects.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-import { onMount } from "svelte";
+  import type { ProjectType } from "../types";
 
-  let { projects } = $props();
+  let { projects }: { projects: Array<ProjectType> } = $props();
 
   let activeProject = $state(0);
-  let projectList;
+  let mouseY = $state(0);
+  let mouseX = $state(0);
+  let projectList: HTMLUListElement;
 
   const handlePrev = () => {
     if (activeProject === 0) {
@@ -22,6 +24,11 @@ import { onMount } from "svelte";
     }
   };
 
+  const handleMouseMove = (e) => {
+    mouseY = e.clientY;
+    mouseX = e.clientX;
+  };
+
   $effect(() => {
     if (projectList) {
       projectList.scrollTo({
@@ -29,40 +36,39 @@ import { onMount } from "svelte";
       });
     }
   });
-
-  onMount(() => {
-    const projectHeaders = projectList.querySelectorAll("header");
-
-    window.addEventListener("mousemove", (e) => {
-      projectHeaders.forEach((header) => {
-        header.style.transform = `translateY(${e.clientY}px)`;
-      });
-    });
-  })
 </script>
+
+<svelte:window on:mousemove={handleMouseMove} />
 
 <section class="w-full h-full">
   <ul bind:this={projectList} class="w-full h-full overflow-hidden">
     {#each projects as project, projectIndex}
-        <li class="w-full h-full text-b2">
-          <header class="w-full grid-main h-max pointer-events-none select-none will-change-transform">
-            <span class="flex gap-[0.25ch] col-span-2">
-              <span>{projectIndex + 1}/{projects.length}</span>
-              <h2 class="text-blue">{project.title}</h2>
-              <p>{project.category.join(", ")}</p>
-            </span>
+      <li class="w-full h-full text-b2">
+        <header
+          style="transform: translateY({mouseY}px)"
+          class="w-full grid-main h-max pointer-events-none select-none will-change-transform"
+        >
+          <span class="flex gap-[0.25ch] col-span-2">
+            <span>{projectIndex + 1}/{projects.length}</span>
+            <h2 class="text-blue">{project.title}</h2>
+            <p>{project.category.join(", ")}</p>
+          </span>
 
-            <p class="col-span-2">{project.description}</p>
-            <p>{project.year}</p>
-          </header>
-        </li>
+          <p class="col-span-2">{project.description}</p>
+          <p>{project.year}</p>
+        </header>
+      </li>
     {/each}
   </ul>
 
   <div
     class="fixed top-[var(--logo-desktop)] left-0 w-full h-[calc(100%-var(--logo-desktop)-var(--footer-height))] grid-main"
   >
-    <button class="col-start-1 col-span-1" onclick={handlePrev}></button>
-    <button class="col-start-5 col-span-1" onclick={handleNext}></button>
+    <button class="col-start-1 col-span-1" onclick={handlePrev}>
+      <span class="fixed link">Prev</span>
+    </button>
+    <button class="col-start-5 col-span-1" onclick={handleNext}>
+      <span class="fixed link">Next</span>
+    </button>
   </div>
 </section>
---
+++ astro/src/types/index.d.ts
@@ -0,0 +1,7 @@
+export interface ProjectType {
+  id: number;
+  title: string;
+  category: string[];
+  description: string;
+  year: number;
+}
