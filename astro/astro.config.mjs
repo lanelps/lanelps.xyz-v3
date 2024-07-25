48bbbe5918fb6311f5db007749a1bd894e274c56
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-07-26 00:28:25 +1000

    feat: Update Astro dependencies and add Netlify integration

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
   prefetch: true,
+  output: "server",
+  adapter: netlify()
 });
