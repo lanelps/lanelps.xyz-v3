6a42eee914d3e568f54a23ae8a05e214577f1095
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-07-29 11:48:46 +1000

    refactor: Update Contact component styling to use inverted colors

--- astro/src/components/Contact.svelte
+++ astro/src/components/Contact.svelte
@@ -19,33 +19,36 @@
     class:pointer-events-auto={contact.isActive}
   >
     <li>
-      <h3 class="inline text-grey">Email</h3>
+      <h3 class="inline text-grey-invert">Email</h3>
       <a
-        class="text-blue-invert"
+        class="text-dark-grey-invert text-stroke-0.2 text-stroke-blue-invert"
         href="mailto:lanelps@gmail.com"
         target="_blank"
         rel="noopener noreferrer">lanelps@gmail.com</a
       >
     </li>
     <li>
-      <h3 class="inline text-grey">Github</h3>
+      <h3 class="inline text-grey-invert">Github</h3>
       <a
+        class="text-dark-grey-invert text-stroke-0.2 text-stroke-blue-invert"
         href="https://github.com/lanelps"
         target="_blank"
         rel="noopener noreferrer">@lanelps</a
       >
     </li>
     <li>
-      <h3 class="inline text-grey">Instagram</h3>
+      <h3 class="inline text-grey-invert">Instagram</h3>
       <a
+        class="text-dark-grey-invert text-stroke-0.2 text-stroke-blue-invert"
         href="https://www.instagram.com/lane_lps/"
         target="_blank"
         rel="noopener noreferrer">@lane_lps</a
       >
     </li>
     <li>
-      <h3 class="inline text-grey">LinkedIn</h3>
+      <h3 class="inline text-grey-invert">LinkedIn</h3>
       <a
+        class="text-dark-grey-invert text-stroke-0.2 text-stroke-blue-invert"
         href="https://www.linkedin.com/in/lane-lps/"
         target="_blank"
         rel="noopener noreferrer">lane-lps</a
@@ -54,6 +57,6 @@
   </ul>
 </section>
 <div
-  class="absolute w-full h-full col-start-3 col-span-full pointer-events-none z-30 bg-white/20 backdrop-blur-sm translate-x-full transition-transform"
+  class="absolute w-full h-full col-start-3 col-span-full pointer-events-none z-30 bg-white/30 backdrop-blur-[32px] translate-x-full transition-transform"
   class:!translate-x-0={contact.isActive}
 ></div>
