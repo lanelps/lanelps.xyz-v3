1039d2ec6fb7d26a4d50f08d9912be87f1944fe8
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-07-29 13:58:09 +1000

    feat: Add @sanity/astro module and update dependencies

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,13 +1,26 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import sanity from "@sanity/astro";
+import { loadEnv } from "vite";
 
-import netlify from "@astrojs/netlify";
+const env = {
+  ...process.env,
+  ...loadEnv(process.env.NODE_ENV, process.cwd(), ["SANITY_"]),
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
+  integrations: [
+    svelte(),
+    tailwind(),
+    sanity({
+      projectId: env.SANITY_PROJECT_ID,
+      dataset: env.SANITY_DATASET,
+      token: env.SANITY_TOKEN,
+      useCdn: false,
+    }),
+  ],
   prefetch: true,
   output: "server",
-  adapter: netlify()
 });
