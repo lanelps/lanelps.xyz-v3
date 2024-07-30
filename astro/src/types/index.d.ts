f143d3ae50bca2038d74e08ee63176364ccfd18c
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-07-30 15:25:45 +1000

    refactor: Update Image interface in index.d.ts for better image handling

--- astro/src/types/index.d.ts
+++ astro/src/types/index.d.ts
@@ -1,6 +1,7 @@
 import type { ImageUrlBuilder } from "@sanity/image-url/lib/types/builder";
 
-export interface Image {
+export interface RawImage {
+  alt?: string;
   asset: {
     _ref: string;
   };
@@ -10,69 +11,20 @@ export interface Image {
     top: number;
     bottom: number;
   };
+  hotspot?: {
+    x: number;
+    y: number;
+    height: number;
+    width: number;
+  };
   mobile?: {
     asset: {
       _ref: string;
     };
-    crop?: {
-      left: number;
-      right: number;
-      top: number;
-      bottom: number;
-    };
   };
 }
 
-export interface ProjectType {
-  id: number;
-  title: string;
-  category: string[];
-  description: string;
-  year: number;
-}
-
-export interface Project {
-  slug: { current: string };
-  title: string;
-}
-
-export type UrlFor = (
-  imgRef: Image,
-  options?: { quality?: number }
-) => ImageUrlBuilder;
-
-export type GetImageDimensions = (image: Image) => {
-  width: number;
-  height: number;
-  aspectRatio: number;
-};
-
-export type GetRetinaSizes = (
-  baseSizes: number[],
-  imageWidth: number,
-  maxWidth: number,
-  minimumWidthStep: number
-) => number[];
-
-export type ProcessImage = (
-  img: Image,
-  maxWidth: number
-) => {
-  src: string;
-  srcset: string;
-  sizes: string;
-  width: number;
-  height: number;
-  aspectRatio: number;
-  placeholder: string;
-};
-
-export type GetImageProps = (props: {
-  image: Image;
-  maxWidth: number;
-  minimumWidthStep?: number;
-  customWidthSteps?: number[];
-}) => {
+export interface Image {
   src: string;
   srcset: string;
   sizes: string;
@@ -91,4 +43,69 @@ export type GetImageProps = (props: {
         placeholder: string;
       }
     | undefined;
+}
+
+export interface Video {
+  public_id: string;
+  width: number;
+  height: number;
+  url: string;
+}
+
+export interface Media {
+  _key?: string | undefined;
+  type: "image" | "video";
+  image?: RawImage;
+  video?: Video;
+  layout: "full" | "center" | "left" | "right";
+}
+
+export interface Project {
+  _id: string;
+  slug: { current: string };
+  title: string;
+  description: string;
+  role: string;
+  collaborators: string;
+  software: string;
+  cover: Media;
+  gallery: Media[];
+}
+
+export type UrlFor = (
+  imgRef: RawImage,
+  options?: { quality?: number }
+) => ImageUrlBuilder;
+
+export type GetImageDimensions = (image: RawImage) => {
+  width: number;
+  height: number;
+  aspectRatio: number;
 };
+
+export type GetRetinaSizes = (
+  baseSizes: number[],
+  imageWidth: number,
+  maxWidth: number,
+  minimumWidthStep: number
+) => number[];
+
+export type ProcessImage = (
+  img: RawImage,
+  maxWidth: number
+) => {
+  src: string;
+  srcset: string;
+  sizes: string;
+  width: number;
+  height: number;
+  aspectRatio: number;
+  placeholder: string;
+};
+
+export type GetImageProps = (props: {
+  image: RawImage;
+  maxWidth: number;
+  minimumWidthStep?: number;
+  customWidthSteps?: number[];
+}) => Image;
