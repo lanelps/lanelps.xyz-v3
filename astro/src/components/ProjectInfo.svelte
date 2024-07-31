c0e99ac73afdecf39977ce72414c315c240925c2
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-07-31 14:19:33 +1000

    refactor: Update ProjectInfo component to display project details and descriptions

--- astro/src/components/ProjectInfo.svelte
+++ astro/src/components/ProjectInfo.svelte
@@ -1,6 +1,8 @@
 <script>
   import { project } from "@stores/project.svelte";
 
+  let { description, categories, collaborators, software } = $props();
+
   const handleKeyDown = (event) => {
     if (!project.isActive) return;
 
@@ -12,12 +14,31 @@
 
 <svelte:window on:keydown={handleKeyDown} />
 
-<section
-  class="absolute top-0 w-full h-full col-start-1 col-span-2 pointer-events-none z-40 mix-blend-difference -translate-x-full transition-transform ease-in-out"
-  class:!translate-x-0={project.isActive}
-></section>
+<div class="grid-main pointer-events-none absolute top-0 h-full w-full">
+  <section
+    class="text-b1 pointer-events-auto z-40 col-span-2 col-start-1 grid h-full w-full -translate-x-full grid-cols-subgrid grid-rows-[repeat(4,_fit-content(100%))] place-content-center gap-y-4 mix-blend-difference transition-transform ease-in-out"
+    class:!translate-x-0={project.isActive}
+  >
+    {#if categories.length > 0}
+      <h3 class="text-grey-invert">At a Glance</h3>
+      <p class="text-deep-purple-invert">{categories.join(", ")}</p>
+    {/if}
 
-<div
-  class="absolute top-0 w-full h-full col-start-1 col-span-2 pointer-events-none z-30 bg-white/30 backdrop-blur-[32px] -translate-x-full transition-transform"
-  class:!translate-x-0={project.isActive}
-></div>
+    {#if collaborators.length > 0}
+      <h3 class="text-grey-invert">Collaborators</h3>
+      <p class="text-deep-purple-invert">{collaborators.join(", ")}</p>
+    {/if}
+
+    {#if software.length > 0}
+      <h3 class="text-grey-invert">Software</h3>
+      <p class="text-deep-purple-invert">{software.join(", ")}</p>
+    {/if}
+    <h3 class="text-grey-invert">Description</h3>
+    <p class="text-deep-purple-invert">{description}</p>
+  </section>
+
+  <div
+    class="absolute top-0 z-30 col-span-2 col-start-1 h-full w-full -translate-x-full bg-white/30 backdrop-blur-[32px] transition-transform"
+    class:!translate-x-0={project.isActive}
+  ></div>
+</div>
