9b3c9a5cf704daaa7b99a0a1039f19d83101af9e
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-09-02 14:44:11 +1000

    refactor: Update button text functionality and link styles in FeaturedProjects.svelte

--- astro/src/components/FeaturedProjects.svelte
+++ astro/src/components/FeaturedProjects.svelte
@@ -59,7 +59,7 @@
 
 <section class="h-full w-full sm-t:mt-0">
   <header
-    class="grid-main text-b2 pointer-events-none absolute left-0 top-1/2 z-10 mt-[--logo-mobile] h-max w-full select-none text-grey-invert mix-blend-difference will-change-transform sm-t:mt-0 sm-t:translate-y-1/2"
+    class="grid-main text-b2 pointer-events-none absolute left-0 top-1/2 z-10 h-max w-full select-none text-grey-invert mix-blend-difference will-change-transform sm-t:translate-y-1/2"
   >
     <span class="col-span-3 space-x-[0.25ch] sm-t:col-span-2">
       <span>{activeProject + 1}/{projects.length}</span>
@@ -86,40 +86,53 @@
     {@render children()}
   </ul>
 
-  <div
+  <nav
     class="top-logo h-body grid-main fixed left-0 w-full mix-blend-difference"
   >
     <span
       style="--ty: {mouseY}px; --tx: {mouseX}px"
-      class="link-invert pointer-events-none fixed left-0 top-[3lh] h-max w-max transition-opacity group-hover:opacity-100 sm-t:top-0 sm-t:translate-x-[var(--tx)] sm-t:translate-y-[var(--ty)] sm-t:opacity-0"
+      class="link-invert pointer-events-none fixed left-0 top-0 hidden h-max w-max transition-opacity group-hover:opacity-100 sm-t:block sm-t:translate-x-[var(--tx)] sm-t:translate-y-[var(--ty)] sm-t:opacity-0"
       class:!opacity-100={textActive}>{buttonText}</span
     >
 
     {#if projects.length > 1}
       <button
-        class="group relative col-span-1 col-start-1 block cursor-none"
+        class="group relative col-span-1 col-start-1 block sm-t:cursor-none"
         onclick={handlePrev}
         onmouseenter={() => setButtonText("Prev")}
         onmouseleave={hideButtonText}
-      ></button>
+      >
+        <span
+          class="link-invert pointer-events-none absolute left-0 top-[3lh] sm-t:hidden"
+          >Prev</span
+        >
+      </button>
     {/if}
 
     <a
       href={`/development/${projects[activeProject].slug.current}`}
-      class="group relative col-span-1 col-start-3 block cursor-none"
+      class="group relative col-span-1 col-start-3 block sm-t:cursor-none"
       onmouseenter={() => setButtonText("View")}
       onmouseleave={hideButtonText}
     >
+      <span
+        class="link-invert pointer-events-none absolute left-0 top-[3lh] sm-t:hidden"
+        >View</span
+      >
     </a>
 
     {#if projects.length > 1}
       <button
-        class="group relative col-span-1 block cursor-none sm-t:col-start-5"
+        class="group relative col-span-1 block sm-t:col-start-5 sm-t:cursor-none"
         onclick={handleNext}
         onmouseenter={() => setButtonText("Next")}
         onmouseleave={hideButtonText}
       >
+        <span
+          class="link-invert pointer-events-none absolute right-0 top-[3lh] sm-t:hidden"
+          >Next</span
+        >
       </button>
     {/if}
-  </div>
+  </nav>
 </section>
